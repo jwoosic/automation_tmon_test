f93398703d5049d11fcd150425a16d9faf24ef1b
jwoosic <112223605+jwoosic@users.noreply.github.com>
2022-08-27 23:03:13 +0900

    bigbanner_test_001 빅배너 노출 체크 부분 수정

--- kellytest/src/main/java/kellytest/Bigbanner_test.java
+++ kellytest/src/main/java/kellytest/Bigbanner_test.java
@@ -52,7 +52,7 @@ public class Bigbanner_test {
    
     //빅배너 노출 체크
     private static void bigbannerCheck() {
-        driver.findElement(By.xpath("//android.widget.ImageView[@NAF='[true]'")).click();
+        driver.findElement(By.xpath("//android.widget.ImageView[@NAF='[true]'")).isDisplayed();
         System.out.println("빅배너 노출 확인");
     }
      
